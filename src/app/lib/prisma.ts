3efb5acc64345051b65ccc6a7f5c93000e7c8a6c
ArnabSaga <arnabdey15091@gmail.com>
2026-03-15 00:04:51 +0600

    feat: Initialize Prisma client with a PostgreSQL adapter.

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../generated/prisma/client'
 
-const connectionString = `<%= process.env.DATABASE_URL %>`
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+import { envVars } from "../config/env";
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const connectionString = envVars.DATABASE_URL;
 
-export { prisma }
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
