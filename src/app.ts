7a832bc95c0ab72938f470d290b3cfab6c62b96a
ArnabSaga <arnabdey15091@gmail.com>
2026-03-17 04:18:02 +0600

    feat: Initialize Express application with core middleware and routes, and add the OpsCore Product Requirements Document.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
+import { toNodeHandler } from "better-auth/node";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import path from "path";
 import { envVars } from "./app/config/env";
+import { auth } from "./app/lib/auth";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
 
 const app: Application = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(process.cwd(), `src/app/templates/`));
 
 // parsers
 app.use(express.json());
@@ -18,13 +24,15 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use("/api/auth", toNodeHandler(auth));
+
 app.use(cookieParser());
 
 //* application routes
 app.use("/api/v1", router);
 
 //* Basic route
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_req: Request, res: Response) => {
   res.send("Hello from OpsCore World!");
 });
 
