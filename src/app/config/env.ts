8bc57b37de33d6a803f1810a3e89bbdb601e8a3f
ArnabSaga <arnabdey15091@gmail.com>
2026-03-31 05:15:23 +0600

    Fix(env): comment the SHADOW_DATABASE_URL

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -8,7 +8,7 @@ interface EnvConfig {
   NODE_ENV: string;
   PORT: string;
   DATABASE_URL: string;
-  SHADOW_DATABASE_URL: string;
+  // SHADOW_DATABASE_URL: string;
   BETTER_AUTH_SECRET: string;
   BETTER_AUTH_URL: string;
   FRONTEND_URL: string;
@@ -47,7 +47,7 @@ const loadEnvVariables = (): EnvConfig => {
     "NODE_ENV",
     "PORT",
     "DATABASE_URL",
-    "SHADOW_DATABASE_URL",
+    // "SHADOW_DATABASE_URL",
     "BETTER_AUTH_SECRET",
     "BETTER_AUTH_URL",
     "FRONTEND_URL",
@@ -88,7 +88,7 @@ const loadEnvVariables = (): EnvConfig => {
     NODE_ENV: process.env.NODE_ENV as string,
     PORT: process.env.PORT as string,
     DATABASE_URL: process.env.DATABASE_URL as string,
-    SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL as string,
+    // SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL as string,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
     FRONTEND_URL: process.env.FRONTEND_URL as string,
